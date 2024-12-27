04c081f5f1490b035076e33602c7f64476349c7c
Stachowiak, Bartosz <bartosz.stachowiak@aptiv.com>
2024-12-27 11:20:27 +0100

    remove atexit callback

--- src/event_cnn_minimal/utils/timers.py
+++ src/event_cnn_minimal/utils/timers.py
@@ -1,15 +1,15 @@
-import torch
 import time
-import numpy as np
-import atexit
 from collections import defaultdict
+
+import numpy as np
+import torch
 
 cuda_timers = defaultdict(list)
 timers = defaultdict(list)
 
 
 class CudaTimer:
-    def __init__(self, timer_name=''):
+    def __init__(self, timer_name=""):
         self.timer_name = timer_name
 
         self.start = torch.cuda.Event(enable_timing=True)
@@ -26,7 +26,7 @@ class CudaTimer:
 
 
 class Timer:
-    def __init__(self, timer_name=''):
+    def __init__(self, timer_name=""):
         self.timer_name = timer_name
 
     def __enter__(self):
@@ -41,14 +41,18 @@ class Timer:
 
 
 def print_timing_info():
-    print('== Timing statistics ==')
+    print("== Timing statistics ==")
     for timer_name, timing_values in [*cuda_timers.items(), *timers.items()]:
         timing_value = np.mean(np.array(timing_values))
         if timing_value < 1000.0:
-            print('{}: {:.2f} ms ({} samples)'.format(timer_name, timing_value, len(timing_values)))
+            print(
+                "{}: {:.2f} ms ({} samples)".format(
+                    timer_name, timing_value, len(timing_values)
+                )
+            )
         else:
-            print('{}: {:.2f} s ({} samples)'.format(timer_name, timing_value / 1000.0, len(timing_values)))
-
-
-# this will print all the timer values upon termination of any program that imported this file
-atexit.register(print_timing_info)
+            print(
+                "{}: {:.2f} s ({} samples)".format(
+                    timer_name, timing_value / 1000.0, len(timing_values)
+                )
+            )
